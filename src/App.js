9f1329a22c2e6e43715ec68fea8db19fb6bad152
Anuj Sharma <anuj.sharma01@nagarro.com>
2020-03-02 03:45:57 +0530

    updated code

--- src/App.js
+++ src/App.js
@@ -1,16 +1,40 @@
 import React from 'react';
 import './App.css';
 import './webcomponents/my-resume';
-function App() {
-  return (
-    <div className="container">
-      <h1>Implementing WebComponents inside react</h1>
-      <div>
-        <my-resume url="http://demo9617924.mockable.io/myresume"></my-resume>
-      </div>
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.urlInput = React.createRef();
+    this.state = { url: "http://demo9617924.mockable.io/myresume" };
+  }
+  onShowResume = (e) => {
+    this.setState({ url: this.urlInput.value });
+  }
+  render() {
+    return (
+      <div className="container">
+        <h1>
+          WebComponents inside ReactJs</h1>
+        <br />
+        <div className='row'>
+          <div className='col-sm-6'>
+            <div className="form-group">
 
-    </div>
-  );
+              <input className="form-control" defaultValue={this.state.url} ref={e => this.urlInput = e} placeholder='Enter Resume JSON url here...' />
+              <br />
+            </div>
+          </div>
+          <div className='col-sm-6 text-left'>
+            <button onClick={this.onShowResume} className="btn btn-primary">Load Resume</button>
+          </div>
+        </div>
+        <div>
+          <my-resume url={this.state.url}></my-resume>
+        </div>
+
+      </div >
+    );
+  }
 }
 
 export default App;
--- src/webcomponents/my-resume.js
+++ src/webcomponents/my-resume.js
@@ -14,9 +14,6 @@ class MyResume extends LitElement {
         this.url = '';
         this.isLoading = false;
         this.isError = false;
-        setTimeout(() => {
-            this.url = this.url + '/';
-        }, 1000);
     }
     updated(changedProperties) {
         const url = changedProperties.get('url');
@@ -61,13 +58,30 @@ class MyResume extends LitElement {
         else if (propType === 'object') {
             if (Array.isArray(data)) {
                 return html`<div class='list'>
-                        <h4 class='field-name'>${propName}</h4>
-                        <div class='list-items'>${this.getFullCollection(data)}</div>
+                        <hr/>
+                        <h3 class='field-name'>${propName}</h3>
+                        <hr/>
+                        <div class='list-items'>
+                        ${
+                    data.map(p => {
+                        return this.getFullCollection(p)
+                    })
+                    }
+                        </div>
+                        <br/><br/>
                     </div>`;
             } else {
                 return html`<div class='object'>
-                    <h4 class='field-name'>${propName}</h4>
-                    <div class='object-items'>${this.getFullCollection(data)}</div>
+                    <hr/>
+                    <h3 class='field-name'>${propName}</h3>
+                    <hr/>
+                    <div class='object-items'>
+                        ${
+                    this.getFullCollection(data)
+                    }
+                    </div>
+                        <br/><br/>
+                        </div>
                 </div>`;
             }
         }
@@ -76,13 +90,53 @@ class MyResume extends LitElement {
     render() {
         return html`
             <style>
+            .resume-container{
+                padding: 10px;
+                border: 1px solid #CCC;
+                BORDER-RADIUS: 6PX;
+                border-top: none;
+            }
              .field-name{
                  font-weight:bold;
                  text-transform: capitalize;
+                 color: gray;
+             }
+             h3.field-name{
+                text-align: left;
+                font-size: 24px;
+                padding-left: 30px;
+                color: navy;
+             }
+             hr{
+                border: 0;
+                width  :100%;
+                height: 1px;
+                background: #333;
+                background: -webkit-gradient(linear, left top, right top, color-stop(0%,hsla(0,0%,0%,0)), color-stop(50%,hsla(0,0%,0%,.75)), color-stop(100%,hsla(0,0%,0%,0)));
+                background: -webkit-linear-gradient(left, hsla(0,0%,0%,0) 0%, hsla(0,0%,0%,.75) 50%, hsla(0,0%,0%,0) 100%);
+                background:    -moz-linear-gradient(left, hsla(0,0%,0%,0) 0%, hsla(0,0%,0%,.75) 50%, hsla(0,0%,0%,0) 100%);
+                background:     -ms-linear-gradient(left, hsla(0,0%,0%,0) 0%, hsla(0,0%,0%,.75) 50%, hsla(0,0%,0%,0) 100%);
+                background:      -o-linear-gradient(left, hsla(0,0%,0%,0) 0%, hsla(0,0%,0%,.75) 50%, hsla(0,0%,0%,0) 100%);
+                background:         linear-gradient(left, hsla(0,0%,0%,0) 0%, hsla(0,0%,0%,.75) 50%, hsla(0,0%,0%,0) 100%);
              }
              .field-value{
                  font-style:italic;
-                 font-size:90%;
+                 font-size:70%;
+                 color: gray;
+             }
+             .list-items{
+                padding: 10px 0px 0px 50px;
+                display: inline-block;
+                width: 100%;
+                margin-bottom: 10px;
+             }
+             .object-items{
+                 padding-left:50px;
+             }
+             .item{
+                 margin-bottom:10px;
+                 float: left;
+                width: 50%;
              }
             </style>
             <div>${ this.isLoading ?
