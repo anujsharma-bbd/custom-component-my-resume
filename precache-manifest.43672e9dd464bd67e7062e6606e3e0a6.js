9cfa401fcf7c417599cad1b7a1699e6860da09d9
Anuj Sharma <anuj.sharma01@nagarro.com>
2020-03-02 04:28:50 +0530

    Updates

---
+++ precache-manifest.43672e9dd464bd67e7062e6606e3e0a6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a244165a1f6d7864ca1d12bf60f855d7",
+    "url": "/custom-component-my-resume/index.html"
+  },
+  {
+    "revision": "03d0bf3d9de705e6e0ef",
+    "url": "/custom-component-my-resume/static/css/2.67123b20.chunk.css"
+  },
+  {
+    "revision": "a1c2e66aa7abdb4215d2",
+    "url": "/custom-component-my-resume/static/css/main.fc672ca6.chunk.css"
+  },
+  {
+    "revision": "03d0bf3d9de705e6e0ef",
+    "url": "/custom-component-my-resume/static/js/2.c13d9b00.chunk.js"
+  },
+  {
+    "revision": "8d1d23c112a823e877233124bbc80a59",
+    "url": "/custom-component-my-resume/static/js/2.c13d9b00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a1c2e66aa7abdb4215d2",
+    "url": "/custom-component-my-resume/static/js/main.c106fdea.chunk.js"
+  },
+  {
+    "revision": "1cac6af2c00b39fd175c",
+    "url": "/custom-component-my-resume/static/js/runtime-main.60ecb3bf.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/custom-component-my-resume/precache-manifest.43672e9dd464bd67e7062e6606e3e0a6.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/custom-component-my-resume/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
