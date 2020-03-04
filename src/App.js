f657e56c5a1019ab0fa99e1316b199818cc505fc
Anuj Sharma <sharmaanuj@hotmail.com>
2020-03-04 23:41:55 +0530

    consuming own npm package for rendering json

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import './webcomponents/my-resume';
+import "json-resume";
 class App extends React.Component {
   constructor(props) {
     super(props);
